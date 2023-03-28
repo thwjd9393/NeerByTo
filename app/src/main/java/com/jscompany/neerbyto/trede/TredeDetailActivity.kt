b0d40ba8d9a6f1abfed9cc52c484c6e8a2d4bf9b
thwjd9393 <thwjd9393@naver.com>
2023-03-28 17:25:15 +0900

    update) trede 디테일 페이지 : dot indcator 오픈소스 등록 및 적용, 화면 레이아웃 수정

--- app/src/main/java/com/jscompany/neerbyto/trede/TredeDetailActivity.kt
+++ app/src/main/java/com/jscompany/neerbyto/trede/TredeDetailActivity.kt
@@ -7,6 +7,6 @@ import com.jscompany.neerbyto.R
 class TredeDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_detail)
+        setContentView(R.layout.activity_trede_detail)
     }
 }
