711567aa9d0411ed89b8aa9aa9dec9e7bd83a6cd
thwjd9393 <thwjd9393@naver.com>
2023-04-04 17:49:18 +0900

    회원가입 기능 제작중...

--- app/src/main/java/com/jscompany/neerbyto/login/SingupActivity.kt
+++ app/src/main/java/com/jscompany/neerbyto/login/SingupActivity.kt
@@ -4,32 +4,60 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import com.jscompany.neerbyto.R
 import com.jscompany.neerbyto.databinding.ActivitySingupBinding
 
 class SingupActivity : AppCompatActivity() {
 
-    val binding: ActivitySingupBinding by lazy { ActivitySingupBinding.inflate(layoutInflater) }
+    private val binding: ActivitySingupBinding by lazy { ActivitySingupBinding.inflate(layoutInflater) }
+
+    private val id = binding.inputId.text.toString()
+    private val passwd = binding.inputPasswd.text.toString()
+    private val passwdConf = binding.inputPasswdCheck.text.toString()
+    private val nicName = binding.inputNicname.text.toString()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        init()
+
+        edtextCheck()
+
+        //회원가입 클릭
+        binding.btnSignup.setOnClickListener {clickJoin() }
+
+        binding.etId
+    }
+
+    private fun clickJoin() {
+        //회원가입 버튼 클릭
+        if (binding.inputId.text.toString() != "" && binding.inputPasswd.text.toString() != "" && binding.inputNicname.text.toString() != "") {
+            Toast.makeText(this, "화면이동", Toast.LENGTH_SHORT).show()
+            //db저장
+
+            //startActivity(Intent(this, LocationActivity::class.java))
+        } else {
+            Toast.makeText(this, "입력 하지 않은 항목이 있습니다", Toast.LENGTH_SHORT).show()
+            return
+        }
+    }
+
+    private fun init() {
         //액션바 셋팅
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        //supportActionBar!!.setDisplayShowTitleEnabled(false) //타이틀
         supportActionBar!!.setTitle(R.string.sign_up)
+    }
 
-//        binding.tvConfId.setText(R.string.id_ok)
-//        binding.tvConfId.setTextColor(R.color.purple_700)
 
-        //회원가입 클릭
-        binding.btnSignup.setOnClickListener {
-            startActivity(Intent(this, LocationActivity::class.java))
-        }
+    private fun edtextCheck() {
+
+
 
     }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
