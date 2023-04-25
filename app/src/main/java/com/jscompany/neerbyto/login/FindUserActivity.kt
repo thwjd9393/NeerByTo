c7c9612b62c5284c0a051bc7faa8022b6e6f1d1a
thwjd9393 <thwjd9393@naver.com>
2023-04-25 17:00:22 +0900

    구글 이메일 유료화로 인한 화면 구현 불가

--- app/src/main/java/com/jscompany/neerbyto/login/FindUserActivity.kt
+++ app/src/main/java/com/jscompany/neerbyto/login/FindUserActivity.kt
@@ -1,23 +1,23 @@
 package com.jscompany.neerbyto.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.jscompany.neerbyto.Common
 import com.jscompany.neerbyto.R
+import com.jscompany.neerbyto.RetrofitBaseUrl
 import com.jscompany.neerbyto.databinding.ActivityFindUserBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class FindUserActivity : AppCompatActivity() {
 
     val binding:ActivityFindUserBinding by lazy { ActivityFindUserBinding.inflate(layoutInflater) }
 
-    private val tabUserfind:TabLayout by lazy { binding.tabUserfind }
-
-    private val pager:ViewPager2 by lazy { binding.pagerUserfind }
-
-    var tabTitle = arrayOf("아이디 찾기","비밀번호 찾기") //탭 제목
+    var userId : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,29 +28,47 @@ class FindUserActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true) //왼쪽 뒤로가기 버튼
         supportActionBar!!.setTitle(R.string.find_user) // 타이틀 재설정
 
-        //뷰페이저 연동
-        //1. 아답터 불러오기
-        val findUserAdapter = FindUserAdapter(this) // 아답터 불러오기
 
-        //2. 아답터에 addFragment() 메소드를 이용하여 보여줄 화면(플래그먼트)들 add
-        findUserAdapter.addFragment(FindUserIdFragment())  
-        findUserAdapter.addFragment(FindUserPasswdFragment()) //플래그먼트들 추가
+        binding.btnFindPasswd.setOnClickListener { clickBtn() }
+    }
 
-        //3. 뷰 페이저에 아답터 연결
-        pager.adapter = findUserAdapter
+    //아이디 체크
+    fun checkId() {
+        RetrofitBaseUrl.getRetrofitInstance(Common.dotHomeUrl).create(UserService::class.java)
+            .userIdCheck(userId).enqueue(object : Callback<String>{
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    val result : String = response.body() ?: ""
 
-        //4. add 한 프래그먼트 페이지 붙이기
-        pager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
+                    if(result == "0") {
+                        binding.etId.error = "가입하지 않은 이메일 입니다"
+                    } else {
+                        binding.btnFindPasswd.isClickable = true
+                    }
+
+                }
+
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    Common.makeToast(this@FindUserActivity, getString(R.string.response_server_error))
+                }
+            })
+    }
+
+
+    //이메일 보내기
+    fun clickBtn(){
+
+        AlertDialog.Builder(this).setMessage("임시 비밀번호 보내기")
+            .setPositiveButton("확인") { p0, p1 ->
+                userId = binding.inputId.text.toString()
+                // 임시 비밀번호 담기
+                val imsiPassWd :String = createEmailCode()
+
+
+
             }
-        })
-
-        // 5. TabLayoutMediator 를 이용하여 탭 레이아웃 이름 등록
-        TabLayoutMediator(tabUserfind, pager){ tab, position ->
-            tab.text = tabTitle.get(position).toString()
-        }.attach()
-
+            .setNegativeButton("취소") { p0, p1 ->
+                p0.dismiss()
+            }.show()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -65,5 +83,60 @@ class FindUserActivity : AppCompatActivity() {
 
         return super.onOptionsItemSelected(item)
     }
+
+    private fun createEmailCode(): String { //이메일 인증코드 생성
+        val str = arrayOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+            "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+        var newCode = String()
+        for (x in 0..7) {
+            val random = (Math.random() * str.size).toInt()
+            newCode += str[random]
+        }
+        return newCode
+    }
+
+//    class emailThread(var insiPassWd:String, var userId: String,
+//        var title: String ) : Thread() {
+//
+//        override fun run() {
+//            super.run()
+//
+//            // 보내는 메일 주소와 비밀번호
+//            val username = "dev.sj@gmail.com"
+//            val password = "thgml332!!"
+//
+//            val props = Properties();
+//            props["mail.smtp.auth"] = "true";
+//            props["mail.smtp.starttls.enable"] = "true";
+//            props["mail.smtp.host"] = "smtp.gmail.com";
+//            props["mail.smtp.port"] = "587";
+//
+//            // 비밀번호 인증으로 세션 생성
+//            val session = Session.getInstance(props,
+//                object: javax.mail.Authenticator() {
+//                    override  fun getPasswordAuthentication(): PasswordAuthentication {
+//                        return PasswordAuthentication(username, password);
+//                    }
+//                })
+//
+//            // 메시지 객체 만들기
+//            val message = MimeMessage(session)
+//            message.setFrom(InternetAddress(username))
+//            // 수신자 설정, 여러명으로도 가능
+//            message.setRecipients(Message.RecipientType.TO,
+//                InternetAddress.parse(userId))
+//            message.subject = title
+//            message.setText(insiPassWd)
+//
+//
+//            // 전송
+//            Transport.send(message)
+//
+//        }
+//
+//    }
+
 }
+
+
 
